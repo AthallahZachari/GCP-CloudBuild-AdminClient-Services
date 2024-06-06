b6e16c438aea710b1acad17304deabd69ee59561
Athallah Zachari <azachari007@gmail.com>
2024-06-06 17:54:53 +0700

    update config connection

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
-/* const config = {
-  host: "34.27.26.247",
-  user: "root",
-  password: "{NH1#9byN%ebp;x[",
-  database: "kampus_upnvy",
-}; */
-
 const config = {
+  host: "34.172.217.25",
+  user: "root",
+  password: ">Kq%^L1fulut`Q$#",
+  database: "car_db",//"car_db", 
+}; 
+
+/* const config = {
   host: "localhost",
   user: "root",
   password: "",
   database: "car_db",
-};
+}; */
 
 const connect = mysql.createConnection(config);
 
